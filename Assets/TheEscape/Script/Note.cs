9a9aa31f69d736e1af6bf5c3bb5e31148a18c79b
RacoonProtector <irfan.alici.1998@gmail.com>
2022-05-25 01:14:02 +0200

    SpotLight

--- Assets/TheEscape/Script/Note.cs
+++ Assets/TheEscape/Script/Note.cs
@@ -13,7 +13,7 @@ namespace TheEscape.Script
         {
             SetImageVisible();
         }
-
+        
         public void disconnectInteraction()
         {
             SetImageInvincible();
